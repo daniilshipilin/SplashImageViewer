eaa372679fd2bec6215338d99f66c31725a5afbb
Daniil Shipilin <daniil.shipilin@gmail.com>
2021-10-10 13:22:43 +0200

    Build scripts updated

--- SplashImageViewer/SignAndCreatePackage.bat
+++ SplashImageViewer/SignAndCreatePackage.bat
@@ -4,7 +4,7 @@ set "cert=C:\GitSources\CodeSign\Certificates\Illuminati_Software_Inc_Code_Sign.
 set "timestamp=http://timestamp.digicert.com"
 
 set "bin=.\bin"
-set "src=%bin%\Release"
+set "src=%bin%\Release\net5.0-windows"
 
 signtool.exe sign /fd sha256 /a /f "%cert%" "%src%\Splash.exe"
 signtool.exe timestamp /tr "%timestamp%" /td sha256 "%src%\Splash.exe"
@@ -24,6 +24,7 @@ signtool.exe timestamp /tr "%timestamp%" /td sha256 "%src%\en\Splash.resources.d
 signtool.exe sign /fd sha256 /a /f "%cert%" "%src%\ru\Splash.resources.dll"
 signtool.exe timestamp /tr "%timestamp%" /td sha256 "%src%\ru\Splash.resources.dll"
 
+del /S /Q "%bin%\Splash.zip" >nul 2>&1
 7za.exe a -mx0 -tzip "%bin%\Splash.zip" "%src%\*"
 7za.exe h -scrcSHA256 "%bin%\Splash.zip" > "%bin%\sha256.txt"
 
