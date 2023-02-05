6183a8a79b6d5d200be281ade50a682434a747d5
Daniil Shipilin <daniil.shipilin@gmail.com>
2023-02-05 13:43:44 +0100

    Nuget packages updated & build scripts updated

--- SplashImageViewer/Build.bat
+++ SplashImageViewer/Build.bat
@@ -3,5 +3,6 @@
 del /S /Q .\bin\Release\publish >nul 2>&1
 rmdir /S /Q .\bin\Release\publish >nul 2>&1
 
-dotnet publish -c Release --no-self-contained -o .\bin\Release\publish
+dotnet publish -c Release -o .\bin\Release\publish
+
 pause
--- SplashImageViewer/SignAndCreatePackage.bat
+++ SplashImageViewer/SignAndCreatePackage.bat
@@ -12,9 +12,6 @@ signtool.exe timestamp /tr "%timestamp%" /td sha256 "%src%\Splash.exe"
 signtool.exe sign /fd sha256 /a /f "%cert%" "%src%\Splash.dll"
 signtool.exe timestamp /tr "%timestamp%" /td sha256 "%src%\Splash.dll"
 
-signtool.exe sign /fd sha256 /a /f "%cert%" "%src%\ApplicationUpdater.dll"
-signtool.exe timestamp /tr "%timestamp%" /td sha256 "%src%\ApplicationUpdater.dll"
-
 signtool.exe sign /fd sha256 /a /f "%cert%" "%src%\en\Splash.resources.dll"
 signtool.exe timestamp /tr "%timestamp%" /td sha256 "%src%\en\Splash.resources.dll"
 
